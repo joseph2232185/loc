d7bbd9dc6edd724baa5c4fce76a4fbc3c18e34b4
Joseph22mc <141338932+joseph2232185@users.noreply.github.com>
2024-07-29 12:47:17 -0600

    Add files via upload

---
+++ duc.java
@@ -0,0 +1,6 @@
+//Definición: Spring Boot gestiona las dependencias entre los componentes de la aplicación utilizando el patrón de inyección de dependencias.
+
+//Funcionalidad: Los objetos no crean sus dependencias, sino que se las proporciona un contenedor IoC (Inversion of Control). Esto permite un código más limpio y menos acoplado, facilitando su mantenimiento y pruebas.
+
+//Tipos de Inyección: Spring soporta diferentes tipos de inyección, como la inyección por constructor, la inyección por setter y la inyección de campo. Cada tipo tiene sus ventajas y desventajas, y la elección depende del diseño específico de la aplicación.
+
